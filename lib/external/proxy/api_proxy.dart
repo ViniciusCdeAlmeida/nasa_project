174eb0360e77e4cf38d3acb3e8c0cf93d9bca8ae
Vinícius Corrêa de Almeida <>
2024-04-15 13:16:31 -0300

    fix(proxy): fix api proxy constructor

--- lib/external/proxy/api_proxy.dart
+++ lib/external/proxy/api_proxy.dart
@@ -3,20 +3,23 @@ import 'package:dio/dio.dart';
 class ApiProxy extends DioMixin {
   final List<InterceptorsWrapper>? _interceptorsWrapper;
   final String _apiUrl;
+  final String _apikey;
 
   ApiProxy({
     required String apiUrl,
+    required String apikey,
     List<InterceptorsWrapper>? interceptorsWrapper,
   })  : _interceptorsWrapper = interceptorsWrapper,
-        _apiUrl = apiUrl {
+        _apiUrl = apiUrl,
+        _apikey = apikey {
     _init();
   }
 
   void _init() {
     interceptors.addAll(_interceptorsWrapper ?? []);
 
-    options.listFormat = ListFormat.multiCompatible;
-    options.queryParameters.addAll({'api_key': _apiUrl});
     options.baseUrl = 'http://$_apiUrl';
+
+    options.queryParameters.addAll({'api_key': _apikey});
   }
 }
